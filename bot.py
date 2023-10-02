d0ffc5131c8780a56fa761babb86bb52e67a2ac5
huyikaka <146655667+huyikaka@users.noreply.github.com>
2023-10-02 16:40:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,53 @@
-import os
-from telegram.ext import Updater, CommandHandler
+import logging
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# Get the Telegram bot token from environment variable
-TOKEN = ('5449793938:AAEbBiKgcwFDdF8bx3NRg1tES5TeOypsJLw')
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your Telegram bot.")
+logger = logging.getLogger(__name__)
+
+# Define the function to handle the /start command
+def start(update: Update, context):
+    update.message.reply_text('Hello! I am a join request accepter bot.')
+
+# Define the function to handle new member join requests
+def new_member(update: Update, context):
+    # Get the user ID of the new member
+    user_id = update.message.new_chat_members[0].id
+    
+    # Accept the join request by sending a message with the command /accept_join_request followed by the user ID
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=f'/accept_join_request {user_id}')
+
+# Define the function to handle the /accept_join_request command
+def accept_join_request(update: Update, context):
+    # Get the user ID from the command arguments
+    user_id = context.args[0]
+    
+    # Accept the join request by calling the bot's method to add a user to a chat
+    context.bot.add_chat_member(chat_id=update.effective_chat.id,
+                                user_id=user_id)
+    
+    update.message.reply_text(f'Join request from user {user_id} accepted!')
 
 def main():
-    # Create an instance of the Updater class with your bot token
-    updater = Updater(token=TOKEN, use_context=True)
+    # Create an instance of Updater and pass your bot token
+    updater = Updater("5449793938:AAEbBiKgcwFDdF8bx3NRg1tES5TeOypsJLw", use_context=True)
 
     # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+    dp = updater.dispatcher
 
-    # Register the start handler
-    dispatcher.add_handler(CommandHandler('start', start))
+    # Add handlers for different commands and messages
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_member))
+    dp.add_handler(CommandHandler("accept_join_request", accept_join_request))
 
-    # Start the Bot
-    updater.start_webhook(listen="0.0.0.0", port=int('8443'), url_path=TOKEN)
-    
-    # Set the webhook URL for Heroku deployment
-    updater.bot.setWebhook("https://ghikhhg.herokuapp.com/" + TOKEN)
+    # Start the bot
+    updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
+    # Run the bot until you press Ctrl-C to stop it
     updater.idle()
 
 if __name__ == '__main__':
