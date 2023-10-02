f051faa24c98c79465a597bb92128e5bc46b7251
huyikaka <146655667+huyikaka@users.noreply.github.com>
2023-10-02 10:09:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, MessageHandler, Filters
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -12,53 +12,20 @@ logger = logging.getLogger(__name__)
 referral_dict = {}
 
 # Define the start command handler
-def start(update: Update, context: CallbackContext) -> None:
-    user_id = update.effective_user.id
-    
-    # Check if the user already has a referral code
-    if user_id in referral_dict:
-        referral_code = referral_dict[user_id]
-        update.message.reply_text(f"You already have a referral code: {referral_code}")
-    else:
-        # Generate a new referral code for the user
-        referral_code = generate_referral_code()
-        referral_dict[user_id] = referral_code
-        
-        # Create the referral link with the generated code
-        referral_link = f"https://t.me/your_bot_username?start={referral_code}"
-        
-        update.message.reply_text(f"Your referral code is: {referral_code}\n"
-                                  f"Share this link with your friends to earn rewards: {referral_link}")
+# Define the handler function for the /start command
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your bot.")
 
-# Define the callback query handler for when someone starts using a referral code
-def handle_referral(update: Update, context: CallbackContext) -> None:
-    query = update.callback_query
-    
-    # Get the referred user's ID from the callback data
-    referred_user_id = int(query.data)
-    
-    # Get the referrer's ID from the original message
-    referrer_user_id = query.message.chat_id
-    
-    # Check if the referred user already has a referral code
-    if referred_user_id in referral_dict:
-        query.answer("You have already used a referral code.")
-    else:
-        # Add the referrer's ID as the referred user's referrer
-        referral_dict[referred_user_id] = referrer_user_id
-        
-        query.answer("Referral code successfully used!")
-        
-        # Send a message to the referrer to notify them about the successful referral
-        context.bot.send_message(chat_id=referrer_user_id, text="Congratulations! You have earned a referral reward.")
+# Define the handler function for when a user leaves the channel
+def left_channel(update, context):
+    user_id = update.message.left_chat_member.id
+    context.bot.kick_chat_member(chat_id=update.effective_chat.id, user_id=user_id)
 
-# Generate a random 6-digit alphanumeric referral code
-def generate_referral_code() -> str:
-    import random
-    import string
-    
-    characters = string.ascii_letters + string.digits
-    return ''.join(random.choices(characters, k=6))
+# Add handlers to the dispatcher
+start_handler = MessageHandler(Filters.command & Filters.regex('^/start$'), start)
+left_channel_handler = MessageHandler(Filters.status_update.left_chat_member, left_channel)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(left_channel_handler)
 
 def main() -> None:
     # Create the Updater and pass in your bot token
