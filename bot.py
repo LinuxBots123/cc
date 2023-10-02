76ac7cb99f221d095249ecb3b331c7afe3bbb969
huyikaka <146655667+huyikaka@users.noreply.github.com>
2023-10-02 09:26:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
-import telebot
+from telegram.ext import Updater, MessageHandler, Filters
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-bot = telebot.TeleBot('6554753630:AAG2wV-XPV7mfuD7eMJTxnyEfxsXHLt6PpI')
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token obtained from BotFather
+updater = Updater(token='6554753630:AAG2wV-XPV7mfuD7eMJTxnyEfxsXHLt6PpI', use_context=True)
 
-@bot.channel_post_handler(content_types=['left_chat_member'])
-def handle_left_chat_member(message):
-    user_id = message.left_chat_member.id
-    chat_id = message.chat.id
+def ban_user(update, context):
+    user = update.message.left_chat_member
     
-    # Replace 'YOUR_CHANNEL_ID' with your actual channel ID
-    if chat_id == -1001900546867:  # Check if the user left from your channel
-        bot.kick_chat_member(chat_id, user_id)  # Ban the user from the channel
+    # Replace 'YOUR_CHANNEL_ID' with the ID of your channel
+    channel_id = -1001900546867
+    
+    if update.message.chat_id == int(channel_id):
+        context.bot.kick_chat_member(chat_id=channel_id, user_id=user.id)
 
-# Start the bot
-bot.polling()
+updater.dispatcher.add_handler(MessageHandler(Filters.status_update.left_chat_member, ban_user))
+
+updater.start_polling()
+updater.idle()
