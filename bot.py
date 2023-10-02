2ed196e9814e60ec067b3c5e133b3d4c740ff5af
huyikaka <146655667+huyikaka@users.noreply.github.com>
2023-10-02 16:02:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,37 @@
-from telegram.ext import Updater, MessageHandler, Filters
+import requests
+import time
+import telebot
 
-chat_id = "5488677608"
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+bot = telebot.TeleBot('5449793938:AAEbBiKgcwFDdF8bx3NRg1tES5TeOypsJLw')
 
-def handle_new_post(update, context):
-    channel_username = "LegendxTricks"
-    post_id = update.channel_post.message_id
-    post_url = f"https://t.me/{channel_username}/{post_id}"
+# Replace 'YOUR_CHANNEL_USERNAME' with your actual channel username (e.g., @mychannel)
+channel_username = 'LegendxTricks'
+
+# Replace 'YOUR_CHAT_ID' with the chat ID where you want to send the new post link
+chat_id = '5488677608'
+
+# Function to get the link of the latest post from your channel
+def get_latest_post_link():
+    url = f'https://api.telegram.org/bot{bot.token}/getChat'
+    params = {'chat_id': channel_username}
+    response = requests.get(url, params=params)
+    data = response.json()
     
-    message = f"New post: {post_url}"
-    context.bot.send_message(chat_id=chat_id, text=message)
+    if data['ok']:
+        latest_post_id = data['result']['last_message']['message_id']
+        return f'https://t.me/{channel_username}/{latest_post_id}'
+    
+    return None
 
-# Replace with your bot token
-bot_token = "5449793938:AAFbYnwEHZyx_8JiWjxfUsXBGOursJIMbso"
+# Function to send the latest post link to the specified chat ID
+def send_latest_post_link():
+    latest_post_link = get_latest_post_link()
+    
+    if latest_post_link:
+        bot.send_message(chat_id, latest_post_link)
 
-updater = Updater(bot_token, use_context=True)
-dispatcher = updater.dispatcher
-
-# Register the handler for new channel posts
-dispatcher.add_handler(MessageHandler(Filters.Channel & Filters.update.channel_posts, handle_new_post))
-# Start polling for updates
-updater.start_polling()
+# Main loop to check for new posts every 10 seconds and send them if available
+while True:
+    send_latest_post_link()
+    time.sleep(10)
