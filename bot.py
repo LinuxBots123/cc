e7b036cf9338b40090c66b43fa71e107401ee401
huyikaka <146655667+huyikaka@users.noreply.github.com>
2023-10-02 19:13:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,26 @@
-import telebot
+from telegram.ext import Updater, MessageHandler, Filters
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token obtained from BotFather
-bot = telebot.TeleBot('5449793938:AAFKJYpDRiH18NYeU183SDDUHPWuhSR9dp0')
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+bot_token = '5449793938:AAHM0rf-LCD-FU2VZ-aNEtpq4wgCQPLrUDA'
 
-@bot.message_handler(content_types=['new_chat_members'])
-def handle_new_member(message):
-    # Get the list of new chat members
-    new_members = message.new_chat_members
+def handle_leave(update, context):
+    user_id = update.message.left_chat_member.id
+    chat_id = update.message.chat.id
     
-    # Iterate through each new member and accept their join request
-    for member in new_members:
-        try:
-            bot.restrict_chat_member(message.chat.id, member.id, can_send_messages=True)
-            print(f"Accepted join request for user: {member.username}")
-        except Exception as e:
-            print(f"Failed to accept join request for user: {member.username}. Error: {e}")
+    # Replace 'YOUR_CHANNEL_ID' with your actual channel ID
+    channel_id = 'YOUR_CHANNEL_ID'
+    
+    if chat_id == channel_id:
+        context.bot.kick_chat_member(chat_id, user_id)
 
-# Start the bot
-bot.polling()
+def main():
+    updater = Updater(bot_token, use_context=True)
+    dp = updater.dispatcher
+    
+    dp.add_handler(MessageHandler(Filters.status_update.left_chat_member, handle_leave))
+    
+    updater.start_polling()
+    updater.idle()
+
+if name == 'main':
+    main()
