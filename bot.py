723d71244f12d3d371579d743159999f61ee6d73
huyikaka <146655667+huyikaka@users.noreply.github.com>
2023-10-02 16:49:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,54 +1,20 @@
-import logging
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telebot
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token obtained from BotFather
+bot = telebot.TeleBot('5449793938:AAFKJYpDRiH18NYeU183SDDUHPWuhSR9dp0')
 
-logger = logging.getLogger(__name__)
-
-# Define the function to handle the /start command
-def start(update: Update, context):
-    update.message.reply_text('Hello! I am a join request accepter bot.')
-
-# Define the function to handle new member join requests
-def new_member(update: Update, context):
-    # Get the user ID of the new member
-    user_id = update.message.new_chat_members[0].id
+@bot.message_handler(content_types=['new_chat_members'])
+def handle_new_member(message):
+    # Get the list of new chat members
+    new_members = message.new_chat_members
     
-    # Accept the join request by sending a message with the command /accept_join_request followed by the user ID
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=f'/accept_join_request {user_id}')
+    # Iterate through each new member and accept their join request
+    for member in new_members:
+        try:
+            bot.restrict_chat_member(message.chat.id, member.id, can_send_messages=True)
+            print(f"Accepted join request for user: {member.username}")
+        except Exception as e:
+            print(f"Failed to accept join request for user: {member.username}. Error: {e}")
 
-# Define the function to handle the /accept_join_request command
-def accept_join_request(update: Update, context):
-    # Get the user ID from the command arguments
-    user_id = context.args[0]
-    
-    # Accept the join request by calling the bot's method to add a user to a chat
-    context.bot.add_chat_member(chat_id=update.effective_chat.id,
-                                user_id=user_id)
-    
-    update.message.reply_text(f'Join request from user {user_id} accepted!')
-
-def main():
-    # Create an instance of Updater and pass your bot token
-    updater = Updater("5449793938:AAEbBiKgcwFDdF8bx3NRg1tES5TeOypsJLw", use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # Add handlers for different commands and messages
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_member))
-    dp.add_handler(CommandHandler("accept_join_request", accept_join_request))
-
-    # Start the bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C to stop it
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+# Start the bot
+bot.polling()
