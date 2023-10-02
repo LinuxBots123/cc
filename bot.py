78a74dc5b1729bd3ca222d72d4b29f083cf357e8
huyikaka <146655667+huyikaka@users.noreply.github.com>
2023-10-02 19:17:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,31 @@
+import logging
 from telegram.ext import Updater, MessageHandler, Filters
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-bot_token = '5449793938:AAHM0rf-LCD-FU2VZ-aNEtpq4wgCQPLrUDA'
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
-def handle_leave(update, context):
+# Define your bot token
+TOKEN = '5449793938:AAHM0rf-LCD-FU2VZ-aNEtpq4wgCQPLrUDA'
+
+# Create an updater object
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+# Define the handler function for the /start command
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your bot.")
+
+# Define the handler function for when a user leaves the channel
+def left_channel(update, context):
     user_id = update.message.left_chat_member.id
-    chat_id = update.message.chat.id
-    
-    # Replace 'YOUR_CHANNEL_ID' with your actual channel ID
-    channel_id = '-1001900546867'
-    
-    if chat_id == channel_id:
-        context.bot.kick_chat_member(chat_id, user_id)
+    context.bot.kick_chat_member(chat_id=update.effective_chat.id, user_id=user_id)
 
-def main():
-    updater = Updater(bot_token, use_context=True)
-    dp = updater.dispatcher
-    
-    dp.add_handler(MessageHandler(Filters.status_update.left_chat_member, handle_leave))
-    
-    updater.start_polling()
-    updater.idle()
+# Add handlers to the dispatcher
+start_handler = MessageHandler(Filters.command & Filters.regex('^/start$'), start)
+left_channel_handler = MessageHandler(Filters.status_update.left_chat_member, left_channel)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(left_channel_handler)
 
-if name == 'main':
-    main()
+# Start the bot
+updater.start_polling()
