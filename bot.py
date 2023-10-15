3b0a4bec448d0f1d24eb977f947f8d4000082fc7
huyikaka <146655667+huyikaka@users.noreply.github.com>
2023-10-15 13:22:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import time
 # Replace the values below with your own API credentials
 api_id = 7630000
 api_hash = 'f70361ddf4ec755395b4b6f1ab2d4fae'
-bot_token = '6535562523:AAHEd-aBZFfwzOPC8ZM-H456hl40RtyYhCs'
+bot_token = '6535562523:AAHXFpFc6G-YcMpK6jyTzJHZl8JD3zmHu_I'
 image_paths = ['image/img1.jpeg', 'image/img2.jpeg', 'image/img3.jpeg']
 
 # Create a TelegramClient instance
